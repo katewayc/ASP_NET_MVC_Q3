4566537e429dcee60cc2508c02a9c8d60b0b449e
katewayc <kate.bqool@gmail.com>
2019-04-25 16:29:48 +0800

    ongoing --- ListViewModel

---
+++ source/ASP_NET_MVC_Q3/ViewModels/ListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ASP_NET_MVC_Q3.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_NET_MVC_Q3.ViewModels
+{
+    public class ListViewModel
+    {
+        public Product product { get; set; }
+        public IEnumerable<ProductView> ProductList { get; set; }
+
+        public class ProductView : Product
+        {
+            public string FormatedCreateTime { get; set; }
+            public string FormatedUpdateTime { get; set; }
+        }
+    }
+}
